c4ce46af30d22acf0a53e726dc0bc5308b8865ad
leonardoXsamuel <leonardosmsilva2004@gmail.com>
2025-05-14 09:00:42 -0300

    Update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -3,6 +3,7 @@ create database if not exists emergia;
 
 use emergia;
 
+-- periodo: dia, mês, ano...
 create table periodo (
 id int not null,
 dias int,
@@ -10,12 +11,14 @@ nome varchar(16),
 primary key (id));
 insert into periodo (id, dias, nome) values (1, 1, 'dia'), (2, 30, 'mês'), (3, 365, 'ano');
 
+-- unidades de medida: kWh, m, m¹, m², ton...
 create table unidade (
 id int not null,
 nome varchar(16),
 primary key (id));
 insert into unidade (id, nome) values (1, 'm'), (2, 'm2'), (3, 'm3'), (4, 'ton'), (5, 'kWh'), (6, 'litro'), (7, 'dólar');
 
+-- recursos e suas transformidaes:
 create table componente (
 id int not null auto_increment,
 titulo varchar(64),
@@ -27,12 +30,14 @@ foreign key (periodoId) references periodo(id)ON DELETE CASCADE,
 primary key (id)
 );
 
+-- Tipos de sistema: fundição, construção
 create table sistema (
 id int not null auto_increment,
 titulo varchar(128),
 primary key (id)
 );
 
+-- relaciona quais componentes pertencem a quais sistemas...
 create table sistema_componente (
 id int not null auto_increment,
 sistemaId int,
